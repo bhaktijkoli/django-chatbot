9df15d98ad4d40c246e5286224960b0262e23009
bhagyarsh dhumal <bhagyarsh@gmail.com>
2018-11-13 18:35:48 +0530

    css linked

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b*hu-viabm)j$!-fd=r+&ra7n3dzxi*3p(v8y0y-h8_4vx$(bj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'accounts'
+    'accounts',
+    'web'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "project-static"),
+
+]
+
 from .rest_settings import *
--- chatbot/urls.py
+++ chatbot/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 import accounts
 urlpatterns = [
+    path('', include('web.urls')),
     path('admin/', admin.site.urls),
+
     path('api-auth/', include('rest_framework.urls')),
 
     path('api/v1/', include('accounts.api.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
